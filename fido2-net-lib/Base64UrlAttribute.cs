141baf20e41458f141ef30ef0c5aced8ba578018
Anders Åberg <anders.aberg@caspeco.se>
2018-08-05 19:30:20 +0200

    Base64url jsonconverter

---
+++ fido2-net-lib/Base64UrlAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace fido2NetLib
+{
+    public class Base64UrlConverter : JsonConverter<byte[]>
+    {
+        public override void WriteJson(JsonWriter writer, byte[] value, JsonSerializer serializer)
+        {
+            writer.WriteValue(Base64Url.Encode(value));
+        }
+
+        public override byte[] ReadJson(JsonReader reader, Type objectType, byte[] existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            return Base64Url.Decode((string)reader.Value);
+        }
+    }
+}
