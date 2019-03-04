eb575952202dd52115656b716a01d0389efa87cf
Anders Åberg <anders.aberg@caspeco.se>
2019-03-04 21:47:41 +0100

    Added more documentation and instant.js

---
+++ Fido2Demo/UrlHelperExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fido2Demo
+{
+    public static class UrlHelperExtensions
+    {
+        public static string ToGithub(this IUrlHelper url, string path)
+        {
+            return "https://github.com/abergs/fido2-net-lib/blob/design/" + path;
+        }
+    }
+}
