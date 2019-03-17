fdf74186f70d266a82dbcf53a48e8c9d69d2682c
Anders Åberg <anders.aberg@caspeco.se>
2019-03-17 13:36:48 +0100

    Added Custom page with options

---
+++ Fido2Demo/Pages/custom.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fido2Demo
+{
+    public class CustomModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
