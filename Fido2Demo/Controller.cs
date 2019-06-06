c78e9c5739f468189e103131789d52dd6dd16767
Alex Seigler <alexseigler@hotmail.com>
2019-06-06 10:38:52 -0400

    Remove AppID extension from assertion options path
    Resolves #92.

--- Fido2Demo/Controller.cs
+++ Fido2Demo/Controller.cs
@@ -239,7 +239,7 @@ namespace Fido2Demo
                 // 2. Get registered credentials from database
                 var existingCredentials = DemoStorage.GetCredentialsByUser(user).Select(c => c.Descriptor).ToList();
 
-                var exts = new AuthenticationExtensionsClientInputs() { AppID = _origin, SimpleTransactionAuthorization = "FIDO", GenericTransactionAuthorization = new TxAuthGenericArg { ContentType = "text/plain", Content = new byte[] { 0x46, 0x49, 0x44, 0x4F } }, UserVerificationIndex = true, Location = true, UserVerificationMethod = true };
+                var exts = new AuthenticationExtensionsClientInputs() { SimpleTransactionAuthorization = "FIDO", GenericTransactionAuthorization = new TxAuthGenericArg { ContentType = "text/plain", Content = new byte[] { 0x46, 0x49, 0x44, 0x4F } }, UserVerificationIndex = true, Location = true, UserVerificationMethod = true };
 
                 // 3. Create options
                 var uv = string.IsNullOrEmpty(userVerification) ? UserVerificationRequirement.Discouraged : userVerification.ToEnum<UserVerificationRequirement>();
