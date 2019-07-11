4f2da32dc3f08c7ecaa6bdb3ef7fc0e5e79811ed
Alex Seigler <alexseigler@hotmail.com>
2019-07-10 20:28:59 -0400

    Restructure repository per proposal in #79 (#95)
    * Restructure repository per proposal in #79. Resolves #79.
    
    * Move external libs back out of src directory

--- Scripts/packRelease.ps1
+++ Scripts/packRelease.ps1
@@ -1,5 +1,5 @@
 $scriptDir = Split-Path $myinvocation.mycommand.path -Parent
 $root = Split-Path $scriptDir -Parent
-$target = Join-Path $root "fido2-net-lib" | Join-Path -ChildPath "Fido2NetLib.csproj"
+$target = Join-Path $root "Src" | Join-Path -ChildPath "Fido2NetLib.csproj"
 Write-Host "Packing $target"
 dotnet pack -c Release $target
