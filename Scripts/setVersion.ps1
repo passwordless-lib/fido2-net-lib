a911d35399e14f4eaf2752a72536f928ea77e5a1
Anders Åberg <anders.aberg@caspeco.se>
2019-07-24 17:34:42 +0200

    Added buildscripts and bumped versions

---
+++ Scripts/setVersion.ps1
@@ -0,0 +1,29 @@
+param([String]$VersionPrefix="1.0.0", [String]$VersionSuffix="") #Must be the first statement in your script
+
+$scriptDir = Split-Path $myinvocation.mycommand.path -Parent
+$root = Split-Path $scriptDir -Parent
+
+$target1 = Join-Path $root "Src" | Join-Path -ChildPath "Fido2NetLib.csproj"
+$target2 = Join-Path $root "Fido2NetLib.Models" | Join-Path -ChildPath "Fido2NetLib.Models.csproj"
+
+Write-Host "Changing $target1"
+$filePathToTask = $target1
+$xml = New-Object XML
+$xml.Load($filePathToTask)
+$element =  $xml.SelectSingleNode("//VersionPrefix")
+$element.InnerText = $VersionPrefix
+$element =  $xml.SelectSingleNode("//VersionSuffix")
+$element.InnerText = $VersionSuffix
+$xml.Save($filePathToTask)
+
+Write-Host "Changing $target2"
+$filePathToTask = $target2
+$xml = New-Object XML
+$xml.Load($filePathToTask)
+$element =  $xml.SelectSingleNode("//VersionPrefix")
+$element.InnerText = $VersionPrefix
+$element =  $xml.SelectSingleNode("//VersionSuffix")
+$element.InnerText = $VersionSuffix
+$xml.Save($filePathToTask)
+
+Write-Host "Updated to version $VersionPrefix $VersionSuffix"
