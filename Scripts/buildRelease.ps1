87329d59a4d50c46a5946086d090f801e430803f
Anders Åberg <anders.aberg@caspeco.se>
2019-07-24 15:18:04 +0200

    Allow arguments to buildRelease.ps1

--- Scripts/buildRelease.ps1
+++ Scripts/buildRelease.ps1
@@ -2,4 +2,4 @@ $scriptDir = Split-Path $myinvocation.mycommand.path -Parent
 $root = Split-Path $scriptDir -Parent
 $target = Join-Path $root "Src" | Join-Path -ChildPath "Fido2NetLib.csproj"
 Write-Host "Building $target"
-dotnet build -c Release $target
+dotnet build -c Release $target $args
