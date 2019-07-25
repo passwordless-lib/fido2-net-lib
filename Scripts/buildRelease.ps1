1d88a3efbce4bf473f5ed3be80f204bae02ac1c3
Anders Åberg <anders.aberg@caspeco.se>
2019-07-25 07:54:53 +0200

    Added Models to buildRelease script so that CI can test it

--- Scripts/buildRelease.ps1
+++ Scripts/buildRelease.ps1
@@ -1,5 +1,7 @@
 $scriptDir = Split-Path $myinvocation.mycommand.path -Parent
 $root = Split-Path $scriptDir -Parent
-$target = Join-Path $root "Src" | Join-Path -ChildPath "Fido2NetLib.csproj"
-Write-Host "Building $target"
-dotnet build -c Release $target $args
+$target1 = Join-Path $root "Src" | Join-Path -ChildPath "Fido2NetLib.csproj"
+$target2 = Join-Path $root "Models" | Join-Path -ChildPath "Fido2NetLib.Models.csproj"
+Write-Host "Building $target1"
+dotnet build -c Release $target1 $args
+dotnet build -c Release $target2 $args
