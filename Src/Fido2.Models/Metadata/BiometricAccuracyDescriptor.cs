a87081c1162dad9175483a42907dadf4bd2fc85d
Code Therapist <38962170+CodeTherapist@users.noreply.github.com>
2019-10-03 00:24:13 +0200

    Adds xml comment documentation (#122)
    * WIP: Adds xml comment documentation accordingly to the FIDO 2 specification
    
    * Adds xml comment documentation accordingly to the FIDO 2 specification
    
    * Moves types into separeted files but preserve namespace (no breaking change).
    * Renmes Configuration.cs to Fido2Configration.cs to match type name.
    * XML docs for IMetadataService as well.
    
    * Update/Check all documentation against the newer specification

--- Src/Fido2.Models/Metadata/BiometricAccuracyDescriptor.cs
+++ Src/Fido2.Models/Metadata/BiometricAccuracyDescriptor.cs
@@ -2,16 +2,52 @@
 
 namespace Fido2NetLib
 {
+    /// <summary>
+    /// The BiometricAccuracyDescriptor describes relevant accuracy/complexity aspects in the case of a biometric user verification method.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#biometricaccuracydescriptor-dictionary"/>
+    /// </remarks>
     public class BiometricAccuracyDescriptor
     {
+        /// <summary>
+        /// Gets or sets the false rejection rate.
+        /// <para>For example a FRR of 10% would be encoded as 0.1.</para>
+        /// </summary>
+        /// <remarks>
+        ///  [ISO19795-1] for a single template, i.e. the percentage of verification transactions with truthful claims of identity that are incorrectly denied. 
+        /// 
+        /// </remarks>
         [JsonProperty("selfAttestedFRR ")]
         public double SelfAttestedFRR { get; set; }
+        /// <summary>
+        /// Gets or sets the false acceptance rate.
+        /// <para>For example a FAR of 0.002% would be encoded as 0.00002.</para>
+        /// </summary>
         [JsonProperty("selfAttestedFAR ")]
         public double SelfAttestedFAR { get; set; }
+        /// <summary>
+        /// Gets or sets the maximum number of alternative templates from different fingers allowed.
+        /// </summary>
+        /// <remarks>
+        /// For other modalities, multiple parts of the body that can be used interchangeably.
+        /// For example: 3 if the user is allowed to enroll up to 3 different fingers to a fingerprint based authenticator. 
+        /// </remarks>
         [JsonProperty("maxTemplates")]
         public ushort MaxTemplates { get; set; }
+        /// <summary>
+        /// Gets or sets the maximum number of false attempts before the authenticator will block this method (at least for some time).
+        /// <para>Zero (0) means it will never block.</para>
+        /// </summary>
         [JsonProperty("maxRetries")]
         public ushort MaxRetries { get; set; }
+        /// <summary>
+        /// Gets or sets the enforced minimum number of seconds wait time after blocking (e.g. due to forced reboot or similar).
+        /// <para>Zero (0) means that this user verification method will be blocked either permanently or until an alternative user verification method succeeded.</para>
+        /// </summary>
+        /// <remarks>
+        /// All alternative user verification methods MUST be specified appropriately in the metadata in <see cref="MetadataStatement.UserVerificationDetails"/>.
+        /// </remarks>
         [JsonProperty("blockSlowdown")]
         public ushort BlockSlowdown { get; set; }
     }
--- Src/Fido2/IMetadataService.cs
+++ Src/Fido2/IMetadataService.cs
@@ -3,11 +3,34 @@ using System.Threading.Tasks;
 
 namespace Fido2NetLib
 {
+    /// <summary>
+    /// The FIDO2 metadata service.
+    /// </summary>
     public interface IMetadataService
     {
+        /// <summary>
+        /// Gets the metadata TOC (table-of-content) payload entry by a guid.
+        /// </summary>
+        /// <param name="aaguid">The Authenticator Attestation GUID.</param>
+        /// <returns>Returns the entry; Otherwise <c>null</c>.</returns>
         MetadataTOCPayloadEntry GetEntry(Guid aaguid);
+        /// <summary>
+        /// Gets a value indicating whether the internal access token is valid.
+        /// </summary>
+        /// <returns>
+        /// Returns <c>true</c> if access token is valid, or <c>false</c> if the access token is equal to an invalid token value.
+        /// </returns>
         bool ConformanceTesting();
+        /// <summary>
+        /// Gets a value indicating whether the metadata service is initialized.
+        /// </summary>
+        /// <returns>
+        /// Returns <c>true</c> if the metadata service is initialized, or <c>false</c> if the metadata service is not initialized.
+        /// </returns>
         bool IsInitialized();
+        /// <summary>
+        /// Initializes the metadata service.
+        /// </summary>
         Task Initialize();
     }
 }
