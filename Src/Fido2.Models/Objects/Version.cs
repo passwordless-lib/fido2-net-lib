c87d3e1fcb9ae3bb5a1113fda1aebf7ca5383c25
Alex Seigler <alexseigler@hotmail.com>
2019-10-08 13:04:29 -0400

    Minor fixes for passing tests

---
+++ Src/Fido2.Models/Objects/Version.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Fido2NetLib
+{
+    /// <summary>
+    /// Version represents a generic version with major and minor fields.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://fidoalliance.org/specs/fido-uaf-v1.2-rd-20171128/fido-uaf-protocol-v1.2-rd-20171128.html#version-interface"/>
+    /// </remarks>
+
+    public class Version
+    {
+        /// <summary>
+        /// Major version.
+        /// </summary>
+        [JsonProperty("major")]
+        public ushort Major { get; set; }
+
+        /// <summary>
+        /// Minor version.
+        /// </summary>
+        [JsonProperty("minor")]
+        public ushort Minor { get; set; }
+    }
+}
