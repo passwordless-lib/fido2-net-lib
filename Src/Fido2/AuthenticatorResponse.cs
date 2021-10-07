c995e20d8c5e0d0357004acf4bfb2a02143ff2de
Jason Nelson <jason@carbonmade.com>
2021-10-07 16:58:25 -0700

    Remove TokenBinding

--- Src/Fido2/AuthenticatorResponse.cs
+++ Src/Fido2/AuthenticatorResponse.cs
@@ -35,8 +35,6 @@ namespace Fido2NetLib
             Type = response.Type;
             Challenge = response.Challenge;
             Origin = response.Origin;
-            TokenBinding = response.TokenBinding;
-
         }
 
 #nullable disable
@@ -52,8 +50,6 @@ namespace Fido2NetLib
         [JsonConverter(typeof(Base64UrlConverter))]
         public byte[] Challenge { get; set; }
         public string Origin { get; set; }
-
-        public TokenBindingDto TokenBinding { get; set; }
 
         // todo: add TokenBinding https://www.w3.org/TR/webauthn/#dictdef-tokenbinding
 
@@ -76,12 +72,6 @@ namespace Fido2NetLib
             if (!string.Equals(fullyQualifiedOrigin, fullyQualifiedExpectedOrigin, StringComparison.OrdinalIgnoreCase))
                 throw new Fido2VerificationException($"Fully qualified origin {fullyQualifiedOrigin} of {Origin} not equal to fully qualified original origin {fullyQualifiedExpectedOrigin} of {expectedOrigin}");
 
-            // 6. Verify that the value of C.tokenBinding.status matches the state of Token Binding for the TLS connection over which the assertion was obtained. 
-            // If Token Binding was used on that TLS connection, also verify that C.tokenBinding.id matches the base64url encoding of the Token Binding ID for the connection.
-            if (TokenBinding != null)
-            {
-                TokenBinding.Verify(requestTokenBindingId);
-            }
         }
 
         private string FullyQualifiedOrigin(string origin)
