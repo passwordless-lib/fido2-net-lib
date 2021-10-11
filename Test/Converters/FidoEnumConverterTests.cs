cf2a73213f80ebe0ec1a9e6d2c81274fc497cd0f
Jason Nelson <jason@carbonmade.com>
2021-10-11 10:17:54 -0700

    Add FidoEnumConverter test coverage

---
+++ Test/Converters/FidoEnumConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+
+using Fido2NetLib;
+using Fido2NetLib.Objects;
+
+using Xunit;
+
+namespace Test.Converters
+{
+    public class FidoEnumConverterTests
+    {
+        [Fact]
+        public void CorrectlyUsesEnumMemberValue()
+        {
+            Assert.Equal("\"secure_element\"", JsonSerializer.Serialize(KeyProtection.SECURE_ELEMENT));
+            Assert.Equal(KeyProtection.SECURE_ELEMENT, JsonSerializer.Deserialize<KeyProtection>("\"secure_element\""));
+
+            Assert.Equal("\"public-key\"", JsonSerializer.Serialize(PublicKeyCredentialType.PublicKey));
+            Assert.Equal(PublicKeyCredentialType.PublicKey, JsonSerializer.Deserialize<PublicKeyCredentialType>("\"public-key\""));
+        }
+    }
+}
