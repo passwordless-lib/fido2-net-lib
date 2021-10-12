65744bc4c3a34aca5bdfc433c76cf88ef8c8945c
Jason Nelson <jason@carbonmade.com>
2021-10-12 14:47:32 -0700

    Format DevelopmentMemoryStore

--- Src/Fido2/DevelopmentInMemoryStore.cs
+++ Src/Fido2/DevelopmentInMemoryStore.cs
@@ -25,22 +25,22 @@ namespace Fido2NetLib.Development
 
         public List<StoredCredential> GetCredentialsByUser(Fido2User user)
         {
-            return _storedCredentials.Where(c => c.UserId.SequenceEqual(user.Id)).ToList();
+            return _storedCredentials.Where(c => c.UserId.AsSpan().SequenceEqual(user.Id)).ToList();
         }
 
         public StoredCredential GetCredentialById(byte[] id)
         {
-            return _storedCredentials.Where(c => c.Descriptor.Id.SequenceEqual(id)).FirstOrDefault();
+            return _storedCredentials.FirstOrDefault(c => c.Descriptor.Id.AsSpan().SequenceEqual(id));
         }
 
         public Task<List<StoredCredential>> GetCredentialsByUserHandleAsync(byte[] userHandle)
         {
-            return Task.FromResult(_storedCredentials.Where(c => c.UserHandle.SequenceEqual(userHandle)).ToList());
+            return Task.FromResult(_storedCredentials.Where(c => c.UserHandle.AsSpan().SequenceEqual(userHandle)).ToList());
         }
 
         public void UpdateCounter(byte[] credentialId, uint counter)
         {
-            var cred = _storedCredentials.Where(c => c.Descriptor.Id.SequenceEqual(credentialId)).FirstOrDefault();
+            var cred = _storedCredentials.FirstOrDefault(c => c.Descriptor.Id.AsSpan().SequenceEqual(credentialId));
             cred.SignatureCounter = counter;
         }
 
@@ -53,7 +53,7 @@ namespace Fido2NetLib.Development
         public Task<List<Fido2User>> GetUsersByCredentialIdAsync(byte[] credentialId)
         {
             // our in-mem storage does not allow storing multiple users for a given credentialId. Yours shouldn't either.
-            var cred = _storedCredentials.Where(c => c.Descriptor.Id.SequenceEqual(credentialId)).FirstOrDefault();
+            var cred = _storedCredentials.FirstOrDefault(c => c.Descriptor.Id.AsSpan().SequenceEqual(credentialId));
 
             if (cred is null)
                 return Task.FromResult(new List<Fido2User>());
