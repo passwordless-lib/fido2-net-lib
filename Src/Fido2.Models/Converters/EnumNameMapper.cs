34be16688e639dadefb08f0953b3117f9071a31f
Jason Nelson <jason@carbonmade.com>
2021-10-18 11:41:30 -0700

    Eliminate allocations in EnumExtensions and make public again

---
+++ Src/Fido2.Models/Converters/EnumNameMapper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
+
+namespace Fido2NetLib
+{
+    public static class EnumNameMapper<TEnum>
+        where TEnum: struct, Enum
+    {
+        private static readonly Dictionary<TEnum, string> valueToNames = GetIdToNameMap();
+        private static readonly Dictionary<string, TEnum> namesToValues = Invert(valueToNames);
+
+        private static Dictionary<string, TEnum> Invert(Dictionary<TEnum, string> map)
+        {
+            var result = new Dictionary<string, TEnum>(map.Count, StringComparer.OrdinalIgnoreCase);
+
+            foreach (var item in map)
+            {
+                result[item.Value] = item.Key;
+            }
+
+            return result;
+        }
+
+        public static bool TryGetValue(string name, bool ignoreCase, out TEnum value)
+        {
+            if (namesToValues.TryGetValue(name, out value))
+            {
+                if (!ignoreCase && !valueToNames[value].Equals(name, StringComparison.Ordinal))
+                {
+                    value = default;
+
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                value = default;
+
+                return false;
+            }
+        }
+
+        public static bool TryGetValue(string name, out TEnum value)
+        {
+            return namesToValues.TryGetValue(name, out value);
+        }
+
+        public static string GetName(TEnum value)
+        {
+            return valueToNames[value];
+        }
+
+        public static IEnumerable<string> GetNames()
+        {
+            return namesToValues.Keys;
+        }
+
+        private static Dictionary<TEnum, string> GetIdToNameMap()
+        {
+            var dic = new Dictionary<TEnum, string>();
+
+            foreach (var field in typeof(TEnum).GetFields(BindingFlags.Public | BindingFlags.Static))
+            {
+                var description = field.GetCustomAttribute<EnumMemberAttribute>(false);
+
+                var value = (TEnum)field.GetValue(null);
+
+                dic[value] = description is not null ? description.Value : value.ToString();
+            }
+
+            return dic;
+        }
+    }
+}
