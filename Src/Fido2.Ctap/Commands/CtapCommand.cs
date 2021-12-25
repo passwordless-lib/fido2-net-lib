4235687c1231fde103b4ed10e9b431d10a7175fe
Jason Nelson <jason@carbonmade.com>
2021-12-25 01:09:17 -0800

    Add Ctap project

---
+++ Src/Fido2.Ctap/Commands/CtapCommand.cs
@@ -0,0 +1,30 @@
+﻿using Fido2NetLib.Cbor;
+
+namespace Fido2NetLib.Ctap;
+
+public abstract class CtapCommand
+{
+    public abstract CtapCommandType Type { get; }
+
+    protected abstract CborObject? GetObject();
+
+    public byte[] GetPayload()
+    {
+        var @object = GetObject();
+
+        if (@object is null)
+        {
+            return new byte[] { (byte)Type };
+        }
+    
+        var encodedObject = @object.Encode();
+
+        var result = new byte[encodedObject.Length + 1];
+
+        result[0] = (byte)Type;
+
+        encodedObject.AsSpan().CopyTo(result.AsSpan(1));
+
+        return result;      
+    }
+}
---
+++ Src/Fido2.Ctap/Commands/CtapCommandType.cs
@@ -0,0 +1,29 @@
+﻿namespace Fido2NetLib.Ctap;
+
+public enum CtapCommandType : byte
+{
+    AuthenticatorMakeCredential   = 0x01,
+    AuthenticatorGetAssertion     = 0x02,
+    AuthenticatorGetInfo          = 4,
+    AuthenticatorClientPin        = 6,
+    AuthenticatorReset            = 0x07,
+    AuthenticatorGetNextAssertion = 8,
+};
+
+public static class CtapCommandTypeHelper
+{
+    public static string Canonicalize(this CtapCommandType command)
+    {
+        return command switch
+        {
+            CtapCommandType.AuthenticatorMakeCredential   => "authenticatorMakeCredential",
+            CtapCommandType.AuthenticatorGetAssertion     => "authenticatorGetAssertion",
+            CtapCommandType.AuthenticatorGetInfo          => "authenticatorGetInfo",
+            CtapCommandType.AuthenticatorClientPin        => "authenticatorClientPIN",
+            CtapCommandType.AuthenticatorReset            => "authenticatorReset",
+            CtapCommandType.AuthenticatorGetNextAssertion => "authenticatorGetNextAssertion",
+
+            _ => throw new Exception("Invalid command")
+        };
+    }
+}
