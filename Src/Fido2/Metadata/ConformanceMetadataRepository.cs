d93295007bf5f26ba8ae9f3d9711c01069cc1c10
Jason Nelson <jason@carbonmade.com>
2022-05-24 11:49:05 -0700

    Use Ordinal comparer to check string equality

--- Src/Fido2/Metadata/ConformanceMetadataRepository.cs
+++ Src/Fido2/Metadata/ConformanceMetadataRepository.cs
@@ -207,7 +207,7 @@ namespace Fido2NetLib
                 }
 
                 // otherwise we have to manually validate that the root in the chain we are testing is the root we downloaded
-                if (rootCert.Thumbprint == certChain.ChainElements[^1].Certificate.Thumbprint &&
+                if (rootCert.Thumbprint.Equals(certChain.ChainElements[^1].Certificate.Thumbprint, StringComparison.Ordinal) &&
                     // and that the number of elements in the chain accounts for what was in x5c plus the root we added
                     certChain.ChainElements.Count == (blobCertStrings.Length + 1) &&
                     // and that the root cert has exactly one status listed against it
