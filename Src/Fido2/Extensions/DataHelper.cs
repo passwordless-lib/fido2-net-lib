452ff8adcf414a9c31bf213c4ec75052fb7e6cec
Jason Nelson <jason@carbonmade.com>
2022-09-07 08:16:41 -0700

    [Extensions] Use file-scoped namespaces

--- Src/Fido2/Extensions/DataHelper.cs
+++ Src/Fido2/Extensions/DataHelper.cs
@@ -1,50 +1,49 @@
 ﻿using System;
 
-namespace Fido2NetLib
+namespace Fido2NetLib;
+
+internal static class DataHelper
 {
-    internal static class DataHelper
+    public static byte[] Concat(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b)
     {
-        public static byte[] Concat(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b)
-        {
-            var result = new byte[a.Length + b.Length];
+        var result = new byte[a.Length + b.Length];
 
-            a.CopyTo(result);
-            b.CopyTo(result.AsSpan(a.Length));
+        a.CopyTo(result);
+        b.CopyTo(result.AsSpan(a.Length));
 
-            return result;
-        }
+        return result;
+    }
 
-        public static byte[] Concat(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c)
-        {
-            var result = new byte[a.Length + b.Length + c.Length];
+    public static byte[] Concat(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c)
+    {
+        var result = new byte[a.Length + b.Length + c.Length];
 
-            a.CopyTo(result);
-            b.CopyTo(result.AsSpan(a.Length));
-            c.CopyTo(result.AsSpan(a.Length + b.Length));
+        a.CopyTo(result);
+        b.CopyTo(result.AsSpan(a.Length));
+        c.CopyTo(result.AsSpan(a.Length + b.Length));
 
-            return result;
-        }
+        return result;
+    }
 
-        public static byte[] Concat(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c, ReadOnlySpan<byte> d, ReadOnlySpan<byte> e)
-        {
-            var result = new byte[a.Length + b.Length + c.Length + d.Length + e.Length];
+    public static byte[] Concat(ReadOnlySpan<byte> a, ReadOnlySpan<byte> b, ReadOnlySpan<byte> c, ReadOnlySpan<byte> d, ReadOnlySpan<byte> e)
+    {
+        var result = new byte[a.Length + b.Length + c.Length + d.Length + e.Length];
 
-            var position = 0;
-            a.CopyTo(result);
-            position += a.Length;
-            
-            b.CopyTo(result.AsSpan(position));
-            position += b.Length;
+        var position = 0;
+        a.CopyTo(result);
+        position += a.Length;
+        
+        b.CopyTo(result.AsSpan(position));
+        position += b.Length;
 
-            c.CopyTo(result.AsSpan(position));
-            position += c.Length;
+        c.CopyTo(result.AsSpan(position));
+        position += c.Length;
 
-            d.CopyTo(result.AsSpan(position));
-            position += d.Length;
+        d.CopyTo(result.AsSpan(position));
+        position += d.Length;
 
-            e.CopyTo(result.AsSpan(position));
+        e.CopyTo(result.AsSpan(position));
 
-            return result;
-        }
+        return result;
     }
 }
