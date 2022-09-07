700a656474ddc22b94d8e8342d1366395421edbe
Jason Nelson <jason@carbonmade.com>
2022-09-06 19:58:47 -0700

    [Cbor] Use file-scoped namespaces

--- Src/Fido2/Cbor/CborBoolean.cs
+++ Src/Fido2/Cbor/CborBoolean.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 
-namespace Fido2NetLib.Cbor
+namespace Fido2NetLib.Cbor;
+
+public sealed class CborBoolean : CborObject
 {
-    public sealed class CborBoolean : CborObject
+    public static readonly CborBoolean True  = new(true);
+    public static readonly CborBoolean False = new(false);
+
+    public CborBoolean(bool value)
     {
-        public static readonly CborBoolean True  = new(true);
-        public static readonly CborBoolean False = new(false);
-
-        public CborBoolean(bool value)
-        {
-            Value = value;
-        }
-
-        public override CborType Type => CborType.Boolean;
-
-        public bool Value { get; }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Type, Value);
-        }
-
-        public static explicit operator CborBoolean(bool value) => value ? True : False;
-
-        public static implicit operator bool(CborBoolean value) => value.Value;
+        Value = value;
     }
+
+    public override CborType Type => CborType.Boolean;
+
+    public bool Value { get; }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Type, Value);
+    }
+
+    public static explicit operator CborBoolean(bool value) => value ? True : False;
+
+    public static implicit operator bool(CborBoolean value) => value.Value;
 }
