b7c5edb57a072b474e6e6d6932efb6ab4cc1ab6a
Alex Seigler <alexseigler@hotmail.com>
2022-09-08 17:40:40 -0400

    Cleanup

--- Src/Fido2/AttestationFormat/AppleAppAttest.cs
+++ Src/Fido2/AttestationFormat/AppleAppAttest.cs
@@ -3,7 +3,6 @@ using System.Formats.Asn1;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
-using System.Text;
 using Fido2NetLib.Cbor;
 using Fido2NetLib.Objects;
 
@@ -13,7 +12,7 @@ namespace Fido2NetLib
     {
         public static byte[] GetAppleAppIdFromCredCertExtValue(X509ExtensionCollection exts)
         {
-            var appleExtension = exts.Cast<X509Extension>().FirstOrDefault(e => e.Oid!.Value is "1.2.840.113635.100.8.5");
+            var appleExtension = exts.Cast<X509Extension>().FirstOrDefault(static e => e.Oid!.Value is "1.2.840.113635.100.8.5");
 
             if (appleExtension is null || appleExtension.RawData is null)
                 throw new Fido2VerificationException("Extension with OID 1.2.840.113635.100.8.5 not found on Apple AppAttest credCert");
@@ -22,15 +21,12 @@ namespace Fido2NetLib
                 appleAttestationASN.CheckTag(Asn1Tag.Sequence);
                 foreach (Asn1Element s in appleAttestationASN.Sequence)
                 {
-                    switch (s.TagValue)
+                    if (s.TagValue is 1204)
                     {
-                        case 1204:
-                            // App ID is the concatenation of your 10-digit team identifier, a period, and your app's CFBundleIdentifier value 
-                            s.CheckExactSequenceLength(1);
-                            s[0].CheckTag(Asn1Tag.PrimitiveOctetString);
-                            return s[0].GetOctetString();
-                        default:
-                            break;
+                        // App ID is the concatenation of your 10-digit team identifier, a period, and your app's CFBundleIdentifier value 
+                        s.CheckExactSequenceLength(1);
+                        s[0].CheckTag(Asn1Tag.PrimitiveOctetString);
+                        return s[0].GetOctetString();
                     }
                 }
             throw new Fido2VerificationException("Apple AppAttest attestation extension 1.2.840.113635.100.8.5 has invalid data");
@@ -38,7 +34,7 @@ namespace Fido2NetLib
 
         // From https://www.apple.com/certificateauthority/Apple_App_Attestation_Root_CA.pem
         internal static readonly string appleAppAttestationRootCA = "MIICITCCAaegAwIBAgIQC/O+DvHN0uD7jG5yH2IXmDAKBggqhkjOPQQDAzBSMSYwJAYDVQQDDB1BcHBsZSBBcHAgQXR0ZXN0YXRpb24gUm9vdCBDQTETMBEGA1UECgwKQXBwbGUgSW5jLjETMBEGA1UECAwKQ2FsaWZvcm5pYTAeFw0yMDAzMTgxODMyNTNaFw00NTAzMTUwMDAwMDBaMFIxJjAkBgNVBAMMHUFwcGxlIEFwcCBBdHRlc3RhdGlvbiBSb290IENBMRMwEQYDVQQKDApBcHBsZSBJbmMuMRMwEQYDVQQIDApDYWxpZm9ybmlhMHYwEAYHKoZIzj0CAQYFK4EEACIDYgAERTHhmLW07ATaFQIEVwTtT4dyctdhNbJhFs/Ii2FdCgAHGbpphY3+d8qjuDngIN3WVhQUBHAoMeQ/cLiP1sOUtgjqK9auYen1mMEvRq9Sk3Jm5X8U62H+xTD3FE9TgS41o0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBSskRBTM72+aEH/pwyp5frq5eWKoTAOBgNVHQ8BAf8EBAMCAQYwCgYIKoZIzj0EAwMDaAAwZQIwQgFGnByvsiVbpTKwSga0kP0e8EeDS4+sQmTvb7vn53O5+FRXgeLhpJ06ysC5PrOyAjEAp5U4xDgEgllF7En3VcE3iexZZtKeYnpqtijVoyFraWVIyd/dganmrduC1bmTBGwD";
-        
+
         public static readonly X509Certificate2 AppleAppAttestRootCA = new(Convert.FromBase64String(appleAppAttestationRootCA));
 
         // From https://developer.apple.com/documentation/devicecheck/validating_apps_that_connect_to_your_server
@@ -76,7 +72,7 @@ namespace Fido2NetLib
 
             if (!chain.Build(credCert))
             {
-                throw new Fido2VerificationException("Failed to build chain in Apple AppAttest attestation");
+                throw new Fido2VerificationException("Failed to build chain in Apple AppAttest attestation: " + chain.ChainStatus.FirstOrDefault().StatusInformation.ToString());
             }
 
             // 2. Create clientDataHash as the SHA256 hash of the one-time challenge your server sends to your app before performing the attestation, and append that hash to the end of the authenticator data (authData from the decoded object).
@@ -87,15 +83,19 @@ namespace Fido2NetLib
             (var attType, var trustPath) = apple.Verify(attStmt, authenticatorData, clientDataHash);
 
             // 5. Create the SHA256 hash of the public key in credCert, and verify that it matches the key identifier from your app.
-            var keyIdentifer = SHA256.HashData(credCert.GetPublicKey());
-            if (!Convert.FromHexString(credCert.GetNameInfo(X509NameType.SimpleName, false)).SequenceEqual(keyIdentifer))
+            Span<byte> credCertPKHash = stackalloc byte[32];
+            SHA256.HashData(credCert.GetPublicKey(), credCertPKHash);
+            var keyIdentifier = Convert.FromHexString(credCert.GetNameInfo(X509NameType.SimpleName, false));
+            if (!credCertPKHash.SequenceEqual(keyIdentifier))
             {
                 throw new Fido2VerificationException("Public key hash does not match key identifier in Apple AppAttest attestation");
             }
-            
+
             // 6. Compute the SHA256 hash of your app's App ID, and verify that it’s the same as the authenticator data's RP ID hash.
             var appId = GetAppleAppIdFromCredCertExtValue(credCert.Extensions);
-            if (!SHA256.HashData(appId).SequenceEqual(AuthData.RpIdHash))
+            Span<byte> appIdHash = stackalloc byte[32];
+            SHA256.HashData(appId, appIdHash);
+            if (!appIdHash.SequenceEqual(AuthData.RpIdHash))
             {
                 throw new Fido2VerificationException("App ID hash does not match RP ID hash in Apple AppAttest attestation");
             }
@@ -113,7 +113,7 @@ namespace Fido2NetLib
             }
 
             // 9. Verify that the authenticator data's credentialId field is the same as the key identifier.
-            if (!AuthData.AttestedCredentialData.CredentialID.SequenceEqual(keyIdentifer))
+            if (!keyIdentifier.SequenceEqual(AuthData.AttestedCredentialData.CredentialID))
             {
                 throw new Fido2VerificationException("Mismatch between credentialId and keyIdentifier in Apple AppAttest attestation");
             }
