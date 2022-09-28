37bc7e7c01faed2ad9be18498b3d2589309332a4
Jason Nelson <jason@carbonmade.com>
2022-09-28 10:03:06 -0700

    Remove TypedString abstraction and reflection during serialization

---
+++ Src/Fido2/Serialization/AttestationTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Fido2NetLib.Objects;
+
+namespace Fido2NetLib.Serialization;
+
+public sealed class AttestationTypeConverter : JsonConverter<AttestationType>
+{
+    public override AttestationType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return AttestationType.Get(reader.GetString()!);
+    }
+
+    public override void Write(Utf8JsonWriter writer, AttestationType value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.Value);
+    }
+}
