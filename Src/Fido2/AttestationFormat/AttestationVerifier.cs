835978e7ee171c53e2074e834542f8e0097b5a6c
Jason Nelson <jason@carbonmade.com>
2022-12-02 09:52:33 -0800

    Add static AttestationVerifier.Create method

--- Src/Fido2/AttestationFormat/AttestationVerifier.cs
+++ Src/Fido2/AttestationFormat/AttestationVerifier.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 using Fido2NetLib.Cbor;
+using Fido2NetLib.Exceptions;
 using Fido2NetLib.Objects;
 
 namespace Fido2NetLib;
@@ -135,4 +136,20 @@ public abstract class AttestationVerifier
     }
 
     public abstract (AttestationType, X509Certificate2[]) Verify();
+
+    public static AttestationVerifier Create(string formatIdentifier)
+    {
+        return formatIdentifier switch
+        {
+            "none"              => new None(),             // https://www.w3.org/TR/webauthn-2/#sctn-none-attestation
+            "tpm"               => new Tpm(),              // https://www.w3.org/TR/webauthn-2/#sctn-tpm-attestation
+            "android-key"       => new AndroidKey(),       // https://www.w3.org/TR/webauthn-2/#sctn-android-key-attestation
+            "android-safetynet" => new AndroidSafetyNet(), // https://www.w3.org/TR/webauthn-2/#sctn-android-safetynet-attestation
+            "fido-u2f"          => new FidoU2f(),          // https://www.w3.org/TR/webauthn-2/#sctn-fido-u2f-attestation
+            "packed"            => new Packed(),           // https://www.w3.org/TR/webauthn-2/#sctn-packed-attestation
+            "apple"             => new Apple(),            // https://www.w3.org/TR/webauthn-2/#sctn-apple-anonymous-attestation
+            "apple-appattest"   => new AppleAppAttest(),   // https://developer.apple.com/documentation/devicecheck/validating_apps_that_connect_to_your_server  
+            _                   => throw new Fido2VerificationException(Fido2ErrorCode.UnknownAttestationType, $"Unknown attestation type. Was '{formatIdentifier}'")
+        };
+    }
 }
