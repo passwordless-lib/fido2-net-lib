f2d516692c9e70432300b265685f5005b716a2bd
Jason Nelson <jason@carbonmade.com>
2022-12-07 10:58:13 -0800

    Replace MemoryStream + BinaryWriter with IBufferWriter

--- Src/Fido2/Extensions/IBufferWriterExtensions.cs
+++ Src/Fido2/Extensions/IBufferWriterExtensions.cs
@@ -1,12 +1,12 @@
 ﻿using System;
+using System.Buffers;
 using System.Buffers.Binary;
-using System.IO;
 
 namespace Fido2NetLib;
 
-internal static class BinaryWriterExtensions
+internal static class IBufferWriterExtensions
 {
-    public static void WriteUInt16BigEndian(this BinaryWriter writer, ushort value)
+    public static void WriteUInt16BigEndian(this IBufferWriter<byte> writer, ushort value)
     {
         Span<byte> buffer = stackalloc byte[2];
 
@@ -15,7 +15,7 @@ internal static class BinaryWriterExtensions
         writer.Write(buffer);
     }
 
-    public static void WriteUInt32BigEndian(this BinaryWriter writer, uint value)
+    public static void WriteUInt32BigEndian(this IBufferWriter<byte> writer, uint value)
     {
         Span<byte> buffer = stackalloc byte[4];
 
