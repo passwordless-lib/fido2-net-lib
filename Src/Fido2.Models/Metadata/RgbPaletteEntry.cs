25ea515260a96e86e6e48bd2bce81c4308fa563a
Jason Nelson <jason@carbonmade.com>
2023-07-04 13:12:54 -0700

    Make RgbPaletteEntry a readonly struct

--- Src/Fido2.Models/Metadata/RgbPaletteEntry.cs
+++ Src/Fido2.Models/Metadata/RgbPaletteEntry.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
 using System.Text.Json.Serialization;
 
 namespace Fido2NetLib;
@@ -9,23 +9,58 @@ namespace Fido2NetLib;
 /// <remarks>
 /// <see href="https://fidoalliance.org/specs/fido-v2.0-rd-20180702/fido-metadata-statement-v2.0-rd-20180702.html#rgbpaletteentry-dictionary"/>
 /// </remarks>
-public class RgbPaletteEntry
+public readonly struct RgbPaletteEntry : IEquatable<RgbPaletteEntry>
 {
+    [JsonConstructor]
+    public RgbPaletteEntry(ushort r, ushort g, ushort b)
+    {
+        R = r;
+        G = g;
+        B = b;
+    }
+
     /// <summary>
     /// Gets or sets the red channel sample value.
     /// </summary>
-    [JsonPropertyName("r"), Required]
-    public ushort R { get; set; }
+    [JsonPropertyName("r")]
+    public ushort R { get; }
 
     /// <summary>
     /// Gets or sets the green channel sample value.
     /// </summary>
-    [JsonPropertyName("g"), Required]
-    public ushort G { get; set; }
+    [JsonPropertyName("g")]
+    public ushort G { get; }
 
     /// <summary>
     /// Gets or sets the blue channel sample value.
     /// </summary>
-    [JsonPropertyName("b"), Required]
-    public ushort B { get; set; }
+    [JsonPropertyName("b")]
+    public ushort B { get; }
+
+    public bool Equals(RgbPaletteEntry other)
+    {
+        return R == other.R
+            && G == other.G
+            && B == other.B;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is RgbPaletteEntry other && Equals(other);
+    }
+
+    public static bool operator ==(RgbPaletteEntry left, RgbPaletteEntry right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(RgbPaletteEntry left, RgbPaletteEntry right)
+    {
+        return !left.Equals(right);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(R, G, B);
+    }
 }
