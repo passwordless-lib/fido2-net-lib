287b62fa8fb49e3bd54a485f7ca6e782dad6b8fc
Jason Nelson <jason@carbonmade.com>
2023-08-11 10:41:38 -0700

    Make MDSGetEndpointResponse immutable

--- Src/Fido2/Metadata/MDSGetEndpointResponse.cs
+++ Src/Fido2/Metadata/MDSGetEndpointResponse.cs
@@ -1,14 +1,19 @@
-﻿#nullable disable
-
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace Fido2NetLib;
 
 public sealed class MDSGetEndpointResponse
 {
+    [JsonConstructor]
+    public MDSGetEndpointResponse(string status, string[] result)
+    {
+        Status = status;
+        Result = result;
+    }
+
     [JsonPropertyName("status")]
-    public string Status { get; set; }
+    public string Status { get; }
 
     [JsonPropertyName("result")]
-    public string[] Result { get; set; }
+    public string[] Result { get; }
 }
