ad4765107fa85b41cbb3b5a7d7a02a0d16dbcfdd
joegoldman674 <147369450+joegoldman2@users.noreply.github.com>
2023-10-23 07:21:13 -0700

    Remove unnecessary usings

--- Src/Fido2.Models/Objects/MakeNewCredentialResult.cs
+++ Src/Fido2.Models/Objects/MakeNewCredentialResult.cs
@@ -1,11 +1,5 @@
 ﻿#nullable enable
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Fido2NetLib.Objects;
 
 /// <summary>
