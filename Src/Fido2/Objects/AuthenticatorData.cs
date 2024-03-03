59a3870462feab5197ac0cfd7fd49589c91b2ccb
Jason Nelson <jason@carbonmade.com>
2024-03-02 20:47:30 -0800

    Use HashSizeInBytes

--- Src/Fido2/Objects/AuthenticatorData.cs
+++ Src/Fido2/Objects/AuthenticatorData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Buffers;
 using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 
 using Fido2NetLib.Cbor;
 using Fido2NetLib.Exceptions;
@@ -18,9 +19,7 @@ public sealed class AuthenticatorData(
     /// Minimum length of the authenticator data structure.
     /// <see href="https://www.w3.org/TR/webauthn/#sec-authenticator-data"/>
     /// </summary>
-    internal const int MinLength = SHA256HashLenBytes + sizeof(AuthenticatorFlags) + sizeof(uint);
-
-    private const int SHA256HashLenBytes = 32; // 256 bits, 8 bits per byte
+    internal const int MinLength = SHA256.HashSizeInBytes + sizeof(AuthenticatorFlags) + sizeof(uint);
 
     /// <summary>
     /// SHA-256 hash of the RP ID the credential is scoped to.
@@ -101,9 +100,7 @@ public sealed class AuthenticatorData(
         var writer = new ArrayBufferWriter<byte>(512);
 
         writer.Write(RpIdHash);
-
-        writer.Write(stackalloc byte[1] { (byte)_flags });
-
+        writer.Write([(byte)_flags]);
         writer.WriteUInt32BigEndian(SignCount);
 
         if (HasAttestedCredentialData && AttestedCredentialData != null)
@@ -130,7 +127,7 @@ public sealed class AuthenticatorData(
         // Input parsing
         var reader = new MemoryReader(data);
 
-        byte[] rpIdHash = reader.ReadBytes(SHA256HashLenBytes);
+        byte[] rpIdHash = reader.ReadBytes(SHA256.HashSizeInBytes);
 
         var flags = (AuthenticatorFlags)reader.ReadByte();
 
