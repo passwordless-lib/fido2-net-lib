335134656e17c7ac4b150a3d33ce6248e6b368dc
Jason Nelson <jason@carbonmade.com>
2024-03-06 13:25:22 -0800

    Improve nullability annotations for AssertionOptions

--- Src/Fido2.Models/AssertionOptions.cs
+++ Src/Fido2.Models/AssertionOptions.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json;
+﻿#nullable enable
+
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 using Fido2NetLib.Objects;
@@ -12,23 +14,26 @@ namespace Fido2NetLib;
 public class AssertionOptions : Fido2ResponseBase
 {
     /// <summary>
-    /// This member represents a challenge that the selected authenticator signs, along with other data, when producing an authentication assertion.See the §13.1 Cryptographic Challenges security consideration.
+    /// This member represents a challenge that the selected authenticator signs, along with other data, when producing an authentication assertion.
+    /// See the §13.1 Cryptographic Challenges security consideration.
     /// </summary>
     [JsonPropertyName("challenge")]
     [JsonConverter(typeof(Base64UrlConverter))]
-    public byte[] Challenge { get; set; }
+    public byte[]? Challenge { get; set; }
 
     /// <summary>
-    /// This member specifies a time, in milliseconds, that the caller is willing to wait for the call to complete. This is treated as a hint, and MAY be overridden by the client.
+    /// This member specifies a time, in milliseconds, that the caller is willing to wait for the call to complete.
+    /// This is treated as a hint, and MAY be overridden by the client.
     /// </summary>
     [JsonPropertyName("timeout")]
     public ulong Timeout { get; set; }
 
     /// <summary>
-    /// This OPTIONAL member specifies the relying party identifier claimed by the caller.If omitted, its value will be the CredentialsContainer object’s relevant settings object's origin's effective domain
+    /// This OPTIONAL member specifies the relying party identifier claimed by the caller.
+    /// If omitted, its value will be the CredentialsContainer object’s relevant settings object's origin's effective domain
     /// </summary>
     [JsonPropertyName("rpId")]
-    public string RpId { get; set; }
+    public string? RpId { get; set; }
 
     /// <summary>
     /// This OPTIONAL member contains a list of PublicKeyCredentialDescriptor objects representing public key credentials acceptable to the caller, in descending order of the caller’s preference(the first item in the list is the most preferred credential, and so on down the list)
@@ -37,24 +42,26 @@ public class AssertionOptions : Fido2ResponseBase
     public IReadOnlyList<PublicKeyCredentialDescriptor> AllowCredentials { get; set; } = Array.Empty<PublicKeyCredentialDescriptor>();
 
     /// <summary>
-    /// This member describes the Relying Party's requirements regarding user verification for the get() operation. Eligible authenticators are filtered to only those capable of satisfying this requirement
+    /// This member describes the Relying Party's requirements regarding user verification for the get() operation.
+    /// Eligible authenticators are filtered to only those capable of satisfying this requirement
     /// </summary>
     [JsonPropertyName("userVerification")]
     public UserVerificationRequirement? UserVerification { get; set; }
 
     /// <summary>
-    /// This OPTIONAL member contains additional parameters requesting additional processing by the client and authenticator. For example, if transaction confirmation is sought from the user, then the prompt string might be included as an extension.
+    /// This OPTIONAL member contains additional parameters requesting additional processing by the client and authenticator.
+    /// For example, if transaction confirmation is sought from the user, then the prompt string might be included as an extension.
     /// </summary>
     [JsonPropertyName("extensions")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
-    public AuthenticationExtensionsClientInputs Extensions { get; set; }
+    public AuthenticationExtensionsClientInputs? Extensions { get; set; }
 
     public static AssertionOptions Create(
         Fido2Configuration config,
         byte[] challenge,
         IReadOnlyList<PublicKeyCredentialDescriptor> allowedCredentials,
         UserVerificationRequirement? userVerification,
-        AuthenticationExtensionsClientInputs extensions)
+        AuthenticationExtensionsClientInputs? extensions)
     {
         return new AssertionOptions()
         {
@@ -76,6 +83,6 @@ public class AssertionOptions : Fido2ResponseBase
 
     public static AssertionOptions FromJson(string json)
     {
-        return JsonSerializer.Deserialize(json, FidoModelSerializerContext.Default.AssertionOptions);
+        return JsonSerializer.Deserialize(json, FidoModelSerializerContext.Default.AssertionOptions)!;
     }
 }
