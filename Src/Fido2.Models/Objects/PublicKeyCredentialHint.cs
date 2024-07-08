3796805c52ac3efef66fa5423c75b323f6f341eb
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-07-08 09:55:07 +0200

    remove link

--- Src/Fido2.Models/Objects/PublicKeyCredentialHint.cs
+++ Src/Fido2.Models/Objects/PublicKeyCredentialHint.cs
@@ -7,9 +7,6 @@ namespace Fido2NetLib.Objects;
 ///  WebAuthn Relying Parties may use this enumeration to communicate hints to the user-agent about how a request may be best completed. These hints are not requirements, and do not bind the user-agent, but may guide it in providing the best experience by using contextual information that the Relying Party has about the request. Hints are provided in order of decreasing preference so, if two hints are contradictory, the first one controls. Hints may also overlap: if a more-specific hint is defined a Relying Party may still wish to send less specific ones for user-agents that may not recognise the more specific one. In this case the most specific hint should be sent before the less-specific ones.
 /// Hints MAY contradict information contained in credential transports and authenticatorAttachment. When this occurs, the hints take precedence. (Note that transports values are not provided when using discoverable credentials, leaving hints as the only avenue for expressing some aspects of such a request.)
 /// </summary>
-/// <remarks>
-/// https://www.w3.org/TR/webauthn-3/#enum-hints
-/// </remarks>
 [JsonConverter(typeof(FidoEnumConverter<PublicKeyCredentialHint>))]
 public enum PublicKeyCredentialHint
 {
