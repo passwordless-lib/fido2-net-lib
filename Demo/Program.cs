2341521b1ad588bc7998e6cb48df96cc7964aa9c
Anders Åberg <anders@andersaberg.com>
2025-09-02 15:16:09 +0200

    use main as branhc

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -32,6 +32,8 @@ builder.Services.AddFido2(options =>
     options.ServerDomain = builder.Configuration["fido2:serverDomain"];
     options.ServerName = "FIDO2 Test";
     options.Origins = builder.Configuration.GetSection("fido2:origins").Get<HashSet<string>>();
+    
+    // Other options available:
     options.TimestampDriftTolerance = builder.Configuration.GetValue<int>("fido2:timestampDriftTolerance");
     options.MDSCacheDirPath = builder.Configuration["fido2:MDSCacheDirPath"];
     options.BackupEligibleCredentialPolicy = builder.Configuration.GetValue<Fido2Configuration.CredentialBackupPolicy>("fido2:backupEligibleCredentialPolicy");
