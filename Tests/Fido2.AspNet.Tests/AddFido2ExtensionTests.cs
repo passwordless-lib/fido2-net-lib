7d8d117810f90b4db019a1fe3e6aa98f4be8f2c6
Anders Åberg <anders@andersaberg.com>
2025-09-04 09:50:49 +0200

    Add tests

--- Tests/Fido2.AspNet.Tests/AddFido2ExtensionTests.cs
+++ Tests/Fido2.AspNet.Tests/AddFido2ExtensionTests.cs
@@ -95,4 +95,142 @@ public class AddFido2ExtensionTests
         // var mds = serviceProvider.GetService<IMetadataService>();
         // Assert.Null(mds);
     }
+
+    [Fact]
+    public void AddMetadataService_RegistersCustomMetadataService()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var builder = services.AddFido2(config => { });
+
+        // Act
+        builder.AddMetadataService<TestMetadataService>();
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var metadataService = serviceProvider.GetService<IMetadataService>();
+        Assert.NotNull(metadataService);
+        Assert.IsType<TestMetadataService>(metadataService);
+    }
+
+    [Fact]
+    public void AddCachedMetadataService_RegistersCachedService()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddMemoryCache();
+        services.AddSingleton<IDistributedCache, MemoryDistributedCache>();
+        var builder = services.AddFido2(config => { });
+
+        // Act
+        builder.AddCachedMetadataService();
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var metadataService = serviceProvider.GetService<IMetadataService>();
+        Assert.NotNull(metadataService);
+        Assert.IsType<DistributedCacheMetadataService>(metadataService);
+    }
+
+    [Fact]
+    public void AddMetadataRepository_RegistersCustomMetadataRepository()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var builder = services.AddFido2(config => { });
+
+        // Act
+        builder.AddMetadataRepository<TestMetadataRepository>();
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var metadataRepository = serviceProvider.GetService<IMetadataRepository>();
+        Assert.NotNull(metadataRepository);
+        Assert.IsType<TestMetadataRepository>(metadataRepository);
+    }
+
+    [Fact]
+    public void AddFileSystemMetadataRepository_RegistersFileSystemRepository()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var builder = services.AddFido2(config => { });
+        var testPath = "/tmp/test";
+
+        // Act
+        builder.AddFileSystemMetadataRepository(testPath);
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var metadataRepository = serviceProvider.GetService<IMetadataRepository>();
+        Assert.NotNull(metadataRepository);
+        Assert.IsType<FileSystemMetadataRepository>(metadataRepository);
+    }
+
+    [Fact]
+    public void AddConformanceMetadataRepository_RegistersConformanceRepository()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var builder = services.AddFido2(config => { });
+
+        // Act
+        builder.AddConformanceMetadataRepository();
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var metadataRepository = serviceProvider.GetService<IMetadataRepository>();
+        Assert.NotNull(metadataRepository);
+        Assert.IsType<ConformanceMetadataRepository>(metadataRepository);
+    }
+
+    [Fact]
+    public void AddFidoMetadataRepository_RegistersFidoRepository()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var builder = services.AddFido2(config => { });
+
+        // Act
+        builder.AddFidoMetadataRepository();
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var metadataRepository = serviceProvider.GetService<IMetadataRepository>();
+        Assert.NotNull(metadataRepository);
+        Assert.IsType<Fido2MetadataServiceRepository>(metadataRepository);
+    }
+
+    [Fact]
+    public void Fido2NetLibBuilder_Constructor_ThrowsWhenServicesNull()
+    {
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Fido2NetLibBuilder(null));
+    }
+
+    [Fact]
+    public void Fido2NetLibBuilder_ServicesProperty_ReturnsServices()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+
+        // Act
+        var builder = new Fido2NetLibBuilder(services);
+
+        // Assert
+        Assert.Same(services, builder.Services);
+    }
+}
+
+public class TestMetadataService : IMetadataService
+{
+    public bool ConformanceTesting() => false;
+    public Task<MetadataBLOBPayloadEntry> GetEntryAsync(Guid aaguid, CancellationToken cancellationToken = default) => Task.FromResult<MetadataBLOBPayloadEntry>(null);
+}
+
+public class TestMetadataRepository : IMetadataRepository
+{
+    public Task<MetadataBLOBPayload> GetBLOBAsync(CancellationToken cancellationToken = default) => Task.FromResult<MetadataBLOBPayload>(null);
+    public Task<MetadataStatement> GetMetadataStatementAsync(MetadataBLOBPayload blob, MetadataBLOBPayloadEntry entry, CancellationToken cancellationToken = default) => Task.FromResult<MetadataStatement>(null);
 }
