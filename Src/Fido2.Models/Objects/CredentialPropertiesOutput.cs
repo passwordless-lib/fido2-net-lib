252ddd289b61ca15365afc86f24548bc20eeeeb3
Alex Seigler <alexseigler@hotmail.com>
2026-09-05 13:24:45 +0000

    Models: drop credProps.authenticatorDisplayName
    Added to credProps during Level 3 drafting and explicitly removed before
    Recommendation (w3c/webauthn#2187, commit 26ae815041): "Remove
    authenticatorDisplayName from credProps for Level 3... Address the use case in
    Level 4." It is not part of the published specification, so no client will
    ever populate it.

--- Src/Fido2.Models/Objects/CredentialPropertiesOutput.cs
+++ Src/Fido2.Models/Objects/CredentialPropertiesOutput.cs
@@ -12,11 +12,4 @@ public class CredentialPropertiesOutput
     /// </summary>
     [JsonPropertyName("rk")]
     public bool Rk { get; init; }
-
-    /// <summary>
-    /// This OPTIONAL property is a human-palatable description of the credential’s managing authenticator, chosen by the user.
-    /// https://w3c.github.io/webauthn/#dom-credentialpropertiesoutput-authenticatordisplayname
-    /// </summary>
-    [JsonPropertyName("authenticatorDisplayName")]
-    public string? AuthenticatorDisplayName { get; init; }
 }
